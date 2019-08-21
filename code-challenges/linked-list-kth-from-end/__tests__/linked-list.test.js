b81a8863277a454f323baabbb82930461bce9a33
hanna alemu <hanna9alemu@gmail.com>
2019-08-20 23:11:44 -0700

    added test and solution code test doesnt work

---
+++ code-challenges/linked-list-kth-from-end/__tests__/linked-list.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const LinkedList = require('../linked-list');
+
+
+describe('linked-list.js', () => {
+
+  let newList = new LinkedList();
+
+  test('can create an empty list', () => {
+
+    let nullList = new LinkedList();
+    expect(nullList.head).toEqual(null);
+  });
+
+  test('insert into list', () => {
+
+    let nullList = new LinkedList();
+    nullList.insert(1);
+    expect(nullList.head.value).toEqual(1);
+    expect(nullList.head.next).toEqual(null);
+  });
+
+  test('find kth node value', () => {
+
+    let nullList = new LinkedList();
+    nullList.insert(1);
+    nullList.insert(2);
+    nullList.insert(3);
+    nullList.insert(4);
+    nullList.insert(5);
+
+    expect(nullList.kthFromEnd(0)).toEqual(5);
+  });
+
+
+});
