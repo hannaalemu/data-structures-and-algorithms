ff9fe950ce66243717354dee35a53bb987dbafad
hannaalemu <hanna9alemu@outlook.com>
2019-09-06 19:08:07 -0700

    test passed

--- code-challenges/find-leaves-binary-tree/findleaves.test.js
+++ code-challenges/find-leaves-binary-tree/findleaves.test.js
@@ -1,18 +1,40 @@
 'use strict';
 
-const binaryTree = require('./findleaves');
-describe('Binary tree', () => {
-  test('it can initiate a binary tree and append a node', () => {
-    let myTree = new binaryTree();
+const Node = require('./Node');
 
-    const one = new Node(10);
+const pathLength = require('./findleaves');
+const countPathLength = require('./findleaves');
+
+describe('Find path between parent and child', () => {
+  test('It can find the path between a parent and child node', () => {
+
+
+    const one = new Node(1);
     const two = new Node(2);
     const three = new Node(3);
+    const four = new Node(4);
+    const five = new Node(5);
 
-    one.left = two;
-    one.right = three;
+    const six = new Node(6);
+    const seven = new Node(7);
 
-    //myTree.root = new Node(10);
-    console.log(myTree);
+    const eight = new Node(8);
+
+
+    // ----------------------------------------
+    five.left = one;
+    five.right = two;
+
+    one.left = three;
+    two.right = four;
+    two.left = six;
+    four.left = seven;
+    four.right = eight;
+
+    
+    console.log(pathLength(five,5, 8));
+
   });
+
 });
+
--- code-challenges/find-leaves-binary-tree/temp.js
+++ code-challenges/find-leaves-binary-tree/temp.js
@@ -1,33 +1,32 @@
+'use strict';
 
-//     //
-//   append(value) {
-//     let current = this.root;
-//     if(current ==
-//       = null) {
-//       this.root = new Node(value);
-//     }
-//   } 
-    
-    // else {
-    // //     const searchTree = function(current) {
-    //       if(value < current.data) {
-    //         if(current.left === null) {
-    //           current.left = new Node(value);
-    //           return;
-    //         }
-    //         else if(current.left !== null) {
-    //           return searchTree(current.left);
-    //         }
-    //       }
-  
-    //       else if(value > current.data) {
-    //         if(current.right === null) {
-    //           current.right = new Node(value);
-    //           return;
-  
-    //         } else if( current.right !== null) {
-    //           return searchTree(current.right);
-    //         }
-    //       }
-  
-    //     };
+//Hanna - this is the main function to find distance between parent and child nodes
+const pathLength = (root, parent,child) => {
+  if(root === null) {
+    return null;
+  }
+  if(root.value === parent) {
+    return countPathLength(root, child, 0);
+  } else {
+    return pathLength(root.left, parent, child)
+      || pathLength(root.right, parent, child);
+  }
+};
+
+//Hanna - this helper function helps initiate the length counter when we find the parent node
+const countPathLength = (root, child, lengthBetween) => {
+  if(root === null) {
+    return null;
+  }
+
+  //Hanna - this is our base case, when you find the child node, return the path length between parent and child
+  if(root.value === child) {
+    return lengthBetween;
+
+  } else {
+    //Hanna - this is the recursive case. Keep looking until you find the child. Increment the counter by one for each node that you visit.
+    return countPathLength(root.left, child, lengthBetween +1)
+      || countPathLength(root.right,child, lengthBetween + 1 );
+  }
+};
+
